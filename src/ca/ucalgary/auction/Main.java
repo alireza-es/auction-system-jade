bc7f710c427c03acf0a24ef529f38bb8a697fdfd
Alireza Esmaeili <alireza.es@gmail.com>
2023-11-15 16:29:35 -0700

    Build Auction Form

--- src/ca/ucalgary/auction/Main.java
+++ src/ca/ucalgary/auction/Main.java
@@ -4,33 +4,25 @@ import ca.ucalgary.auction.ui.AuctionForm;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 
 public class Main {
     public static void main(String[] args) {
-        AuctionForm form = new AuctionForm();
-        form.setVisible(true);
+        // Get a hold on JADE runtime
+        Runtime runtime = Runtime.instance();
 
-//        // Get a hold on JADE runtime
-//        Runtime rt = Runtime.instance();
-//
-//        // Create a default profile
-//        Profile p = new ProfileImpl();
-//        p.setParameter(Profile.MAIN_HOST, "localhost");
-//        p.setParameter(Profile.MAIN_PORT, "1099");
-//        p.setParameter(Profile.GUI, "true");
-//
-//        // Create a new non-main container, connecting to the default
-//        // main container (i.e., on this host, port 1099)
-//        ContainerController cc = rt.createMainContainer(p);
-//
-//        try {
-//            // Create a new agent, a copy of the original auctioneer
-//            AgentController ac = cc.createNewAgent("auctioneer", "ca.ucalgary.auction.AuctioneerAgent", null);
-//            ac.start();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        // Create a default profile
+        Profile profile = new ProfileImpl();
+        profile.setParameter(Profile.MAIN_HOST, "localhost");
+        profile.setParameter(Profile.MAIN_PORT, "1099");
+        profile.setParameter(Profile.GUI, "true");
+
+        // Create a new non-main container, connecting to the default
+        // main container (i.e., on this host, port 1099)
+        AgentContainer agentContainer = runtime.createMainContainer(profile);
+        AuctionForm form = new AuctionForm(agentContainer);
+        form.setVisible(true);
     }
 }
--- src/ca/ucalgary/auction/ui/AuctionForm.java
+++ src/ca/ucalgary/auction/ui/AuctionForm.java
@@ -1,47 +1,74 @@
 package ca.ucalgary.auction.ui;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+
+import ca.ucalgary.auction.model.Auction;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class AuctionForm extends JFrame {
-    private JTextField itemField;
-    private JTextField priceField;
+    JTable table;
+    DefaultTableModel model;
+    JButton placeBidButton;
+    AgentContainer agentContainer;
+    AgentController auctioneerAgentController;
 
-    public AuctionForm() {
-        setTitle("Start Auction");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 200);
-
-        JPanel panel = new JPanel(new GridLayout(3, 2));
-
-        JLabel itemLabel = new JLabel("Item:");
-        itemField = new JTextField();
-        JLabel priceLabel = new JLabel("Starting Price:");
-        priceField = new JTextField();
-
-        panel.add(itemLabel);
-        panel.add(itemField);
-        panel.add(priceLabel);
-        panel.add(priceField);
-
-        JButton startButton = new JButton("Start Auction");
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                startAuction();
+    public AuctionForm(AgentContainer _agentContainer) {
+        agentContainer = _agentContainer;
+        this.startAuctioneerAgent();
+        model = new DefaultTableModel(new Object[]{"Id", "Item", "Starting Price", "Current Bid", "Bids Count", "Time Left"}, 0);
+        table = new JTable(model);
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+            public void valueChanged(ListSelectionEvent event) {
+                placeBidButton.setEnabled(table.getSelectedRow() != -1);
             }
         });
 
-        panel.add(startButton);
+        JButton startAuctionButton = new JButton("Start Auction");
+        startAuctionButton.addActionListener(e -> startAuction());
 
-        getContentPane().add(panel, BorderLayout.CENTER);
+        placeBidButton = new JButton("Place Bid");
+        placeBidButton.addActionListener(e -> placeBid());
+        placeBidButton.setEnabled(false);
+
+        JPanel panel = new JPanel();
+        panel.add(startAuctionButton);
+        panel.add(placeBidButton);
+
+        add(new JScrollPane(table), BorderLayout.CENTER);
+        add(panel, BorderLayout.SOUTH);
+
+        pack();
+        setVisible(true);
+    }
+
+    public void populateTable(Auction[] auctions) {
+        for (Auction auction : auctions) {
+            model.addRow(new Object[]{auction.getId(), auction.getItem(), auction.getStartingPrice(), auction.getCurrentBid(), auction.getBidsCount(), auction.getTimeLeft()});
+        }
+    }
+
+    private void startAuctioneerAgent() {
+        try {
+            auctioneerAgentController = agentContainer.createNewAgent("auctioneer", "ca.ucalgary.auction.agents.AuctioneerAgent", null);
+            auctioneerAgentController.start();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void startAuction() {
-        String item = itemField.getText();
-        int price = Integer.parseInt(priceField.getText());
-        // Start the auction with the entered item and price
-        System.out.println("Starting auction for item: " + item + " with starting price: " + price);
+        // implement start auction logic
+    }
+
+    private void placeBid() {
+        // implement place bid logic
     }
 }
